5ece9fe95931d1eecb25dea0edc5ccfed57b3b2f
Sergio Cano <canosergio90@gmail.com>
2021-01-14 19:24:52 -0300

    declaring functional component with typescript

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -2,6 +2,20 @@ interface ChildProps {
     color: string
 }
 
+/**
+ * disadvantage: all React componentes can have optionally
+ * these properties: propTypes, displayName, defaultProps, contextTypes
+ * 
+ * Declaring that way, Typescript doesn't now about that and If
+ * we refer to some of these properties, it will complain.
+ * 
+ */
 export const Child = ({ color }: ChildProps) => {
     return <div>{color}</div>
 }
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+    return <div>{color}</div>
+}
+
+ChildAsFC.displayName // now the IDE can help us with autocomplete...

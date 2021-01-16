e0dfa3180d21ae22fbb4410e48a4e5f48f1d3f5b
Sergio Cano <canosergio90@gmail.com>
2021-01-16 15:40:21 -0300

    passing functions to child component and passing children to child comp

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,5 +1,6 @@
 interface ChildProps {
     color: string
+    onClick: () => void
 }
 
 /**
@@ -10,12 +11,19 @@ interface ChildProps {
  * we refer to some of these properties, it will complain.
  * 
  */
-export const Child = ({ color }: ChildProps) => {
-    return <div>{color}</div>
+export const Child = ({ color, onClick }: ChildProps) => {
+    return <div>
+            {color}
+            <button onClick={onClick}>Click me!</button>
+        </div>
 }
 
-export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
-    return <div>{color}</div>
+export const ChildAsFC: React.FC<ChildProps> = ({ color, onClick, children }) => {
+    return <div>
+        {color}
+        {children}
+        <button onClick={onClick}>Click me!</button>
+    </div>
 }
 
 ChildAsFC.displayName // now the IDE can help us with autocomplete...
--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,7 +1,9 @@
-import { Child } from './Child'
+import { ChildAsFC } from './Child'
 
 const Parent = () => {
-    return <Child color="red" />
+    return <ChildAsFC color="red" onClick={ () => console.log('clicked!')} >
+            ImAChildrenProp
+        </ChildAsFC>
 }
 
 export default Parent

e3363335f4534687463e6cad314a6ae4977ac368
Sergio Cano <canosergio90@gmail.com>
2021-01-17 09:38:56 -0300

    more on typing events on event handlers

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -3,8 +3,14 @@ const EventComponent: React.FC = () => {
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         console.log(e)
     }
+
+    const onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+        console.log('im being dragged')
+    }
+
     return <div>
         <input onChange={onChange} />
+        <div draggable onDragStart={onDragStart}>Drag me!</div>
     </div>
 }
 
